4c14d7d5db8e5a1e03693b3557e2b07cebc041ac
KarthigaKK <karthi.kcube2006@gmail.com>
2022-02-04 09:03:49 -0600

    02042020

---
+++ gittxt.py
@@ -0,0 +1,14 @@
+git init
+git status
+git add . (to add all the files)
+git add <filename>
+git commit -m "Comment"
+git remote add origin <repo line>
+git push -u origin master
+
+cd Desktop/
+cd..
+ls
+vi --Write in git editor
+prees "i" to start write
+:wq! to save and exit
